b011b17c42ca4d669c40e7a57a4668f9b220ef7c
vijo30 <jose.videla.araya17@gmail.com>
2022-09-22 22:00:32 -0300

    Se agrega el modelo de el player del juego compuesto en esta etapa por una clase abstracta y una subclase

--- src/main/java/cl/uchile/dcc/finalreality/main/Main.java
+++ src/main/java/cl/uchile/dcc/finalreality/main/Main.java
@@ -1,6 +1,8 @@
 package cl.uchile.dcc.finalreality.main;
 
 
+import cl.uchile.dcc.finalreality.model.weapon.Weapon;
+
 public class Main {
     /**
      * A class that is meant for testing.
@@ -8,6 +10,7 @@ public class Main {
      * @author <a href="https://www.github.com/vijo30">V30</a>
      * @author ~José Videla~
      */
-    public static void main(String args){
+    public static void main(String[] args){
+        System.out.println("Este es un test :D");
     }
 }
---
+++ src/main/java/cl/uchile/dcc/finalreality/model/user/player/Player.java
@@ -0,0 +1,55 @@
+package cl.uchile.dcc.finalreality.model.user.player;
+
+import cl.uchile.dcc.finalreality.model.character.Enemy;
+import cl.uchile.dcc.finalreality.model.character.GameCharacter;
+import cl.uchile.dcc.finalreality.model.user.AbstractPlayer;
+import cl.uchile.dcc.finalreality.model.weapon.Weapon;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * A class that holds all the information of a single player of the game.
+ *
+ * @author <a href="https://www.github.com/vijo30">V30</a>
+ * @author ~José Videla~
+ */
+
+public class Player extends AbstractPlayer {
+
+
+    /**
+     * Creates a new player
+     *
+     * @param party     the player's party
+     * @param enemies   the player's enemies
+     * @param inventory the player's inventory
+     */
+    protected Player(GameCharacter[] party, Enemy[] enemies, Weapon[] inventory) {
+        super(party, enemies, inventory);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Player.class, Arrays.hashCode(party), Arrays.hashCode(enemies), Arrays.hashCode(inventory));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof final Player player)){
+            return false;
+        }
+        return hashCode() == player.hashCode()
+                && party == player.party
+                && enemies == player.enemies
+                && inventory == player.inventory;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
